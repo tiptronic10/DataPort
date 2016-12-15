b6ab0a98645dc46ed569f2118ccb259794aa8762
Cybertronic <cybertronic@126.com>
2016-12-15 17:29:00 +0800

    更新 dataport.h

--- dataport.h
+++ dataport.h
@@ -4,36 +4,41 @@
 #include "dataport_global.h"
 #include "networkdataport.h"
 #include "serialdataport.h"
+#include <QSerialPort>
+#include <QTcpSocket>
+#include <QObject>
 
-class DATAPORTSHARED_EXPORT DataPort
+class DATAPORT_EXPORT DataPort : public QObject
 {
-
+	Q_OBJECT
 public:
-    enum DataPort_Type{
-        NULL_PORT,
-        NETWORK_PORT,
-        SERIAL_PORT
-    };
+	enum DataPort_Type{
+		NULL_PORT,
+		NETWORK_PORT,
+		SERIAL_PORT
+	};
 
-    DataPort();
-    DataPort(DataPort_Type);
-    ~DataPort();
+	DataPort();
+	DataPort(DataPort_Type);
+	~DataPort();
 
-    void createDataPort(DataPort_Type);
-    int getPortType();
+	void createDataPort(DataPort_Type);
+	int getPortType();
 
-    bool open();
-    void write(const QByteArray& data);
-    void close();
+	bool open();
+	void write(const QByteArray& data);
+	void close();
 
 signals:
-    void sig_received(QByteArray data);
-    void sig_error(int);
+	void sig_received(QByteArray data);
+	void sig_error(int);
+	void sig_wirte(QByteArray data);
 
 private:
-    DataPort_Type m_portType;
-    NetworkDataPort* m_networkDataPort;
-    SerialDataPort* m_serialDataPort;
+	QThread* m_thread;
+	DataPort_Type m_portType;
+	NetworkDataPort* m_networkDataPort;
+	SerialDataPort* m_serialDataPort;
 };
 
 #endif // DATAPORT_H

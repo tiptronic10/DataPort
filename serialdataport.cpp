6529d2ef87f6673d19fb30ec993b472becbbdc10
Cybertronic <cybertronic@126.com>
2016-12-15 17:29:41 +0800

    更新 serialdataport.cpp

--- serialdataport.cpp
+++ serialdataport.cpp
@@ -5,22 +5,32 @@ SerialDataPort::SerialDataPort(QObject *parent) : QObject(parent)
 
 }
 
-void SerialDataPort::init()
+void SerialDataPort::slt_init()
+{
+	m_serialPort = new QSerialPort;
+	m_serialPort->open(QIODevice::ReadWrite);
+}
+
+void SerialDataPort::slt_open()
 {
 
 }
 
-void SerialDataPort::open()
+void SerialDataPort::slt_write(const QByteArray& data)
 {
-
+	m_serialPort->write(data);
 }
 
-void SerialDataPort::write(const QByteArray& data)
+void SerialDataPort::slt_read(void)
 {
-    Q_UNUSED(data)
+	if (m_serialPort)
+	{
+		m_receivedData = m_serialPort->readAll();
+		emit sig_received(m_receivedData);
+	}
 }
 
-void SerialDataPort::close()
+void SerialDataPort::slt_close()
 {
-
+	m_serialPort->close();
 }

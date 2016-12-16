bb9503e7a116caee3b87d08863b60ddefd057bc0
Cybertronic <cybertronic@126.com>
2016-12-16 18:02:04 +0800

    更新 dataport.cpp

--- dataport.cpp
+++ dataport.cpp
@@ -1,12 +1,9 @@
 #include "dataport.h"
 
-DataPort::DataPort() : m_portType(DataPort_Type::NULL_PORT),m_thread(nullptr), m_networkDataPort(nullptr), m_serialDataPort(nullptr)
+DataPort::DataPort(DataPort_Type type, const QString& str, const int& num) : m_thread(nullptr), m_networkDataPort(nullptr), m_serialDataPort(nullptr)
 {
-
-}
-
-DataPort::DataPort(DataPort_Type type) : m_thread(nullptr), m_networkDataPort(nullptr), m_serialDataPort(nullptr)
-{
+	m_strData = str;
+	m_numData = num;
 	switch(type)
 	{
 	case DataPort_Type::NULL_PORT:
@@ -20,6 +17,16 @@ DataPort::DataPort(DataPort_Type type) : m_thread(nullptr), m_networkDataPort(nu
 	case DataPort_Type::SERIAL_PORT:
 		m_thread = new QThread;
 		m_serialDataPort = new SerialDataPort();
+		//向串口操作
+		connect(this, SIGNAL(sig_open(QString, int)), m_serialDataPort, SLOT(slt_open(QString, int)));
+		connect(this, SIGNAL(sig_wirte(QByteArray)), m_serialDataPort, SLOT(slt_write(QByteArray)));
+		connect(this, SIGNAL(sig_close()), m_serialDataPort, SLOT(slt_close()));
+		//接收串口信号
+		connect(m_serialDataPort, SIGNAL(sig_received(QByteArray)), this, SIGNAL(sig_received(QByteArray)));//发送接收数据
+		connect(m_serialDataPort, SIGNAL(sig_received(QByteArray)), this, SIGNAL(sig_received()));
+		connect(m_serialDataPort, SIGNAL(sig_error(QSerialPort::SerialPortError)), this, SIGNAL(sig_error(int)));
+		//响应子线程启动信号
+		connect(m_thread, SIGNAL(started()), m_serialDataPort, SLOT(slt_init()));
 		m_serialDataPort->moveToThread(m_thread);
 		m_thread->start();
 		break;
@@ -45,85 +52,22 @@ DataPort::~DataPort()
 	m_portType = DataPort_Type::NULL_PORT;
 }
 
-void DataPort::createDataPort(DataPort_Type type)
-{
-	if(m_portType == DataPort_Type::NULL_PORT)
-	{
-		switch(type)
-		{
-		case DataPort_Type::NULL_PORT:
-			break;
-		case DataPort_Type::NETWORK_PORT:
-			m_thread = new QThread;
-			m_networkDataPort = new NetworkDataPort();
-			m_networkDataPort->moveToThread(m_thread);
-			m_thread->start();
-			break;
-		case DataPort_Type::SERIAL_PORT:
-			m_thread = new QThread;
-			m_serialDataPort = new SerialDataPort();
-			connect(this, SIGNAL(sig_wirte(QByteArray)), m_serialDataPort, SLOT(SerialDataPort::slt_write(QByteArray)));
-			connect(m_thread, SIGNAL(started()), m_serialDataPort, SLOT(SerialDataPort::slt_init()));
-			connect(m_serialDataPort->m_serialPort, SIGNAL(readyRead()), m_serialDataPort, SLOT(SerialDataPort::slt_read()));
-			connect(m_serialDataPort->m_serialPort, SIGNAL(error(QSerialPort::SerialPortError)), this, SLOT());
-			connect(m_serialDataPort, SIGNAL(sig_received(QByteArray)), this, SIGNAL(sig_received(QByteArray)));//发送接收数据
-			m_serialDataPort->moveToThread(m_thread);
-			m_thread->start();
-			break;
-		}
-		m_portType = type;
-	}
-}
-
 int DataPort::getPortType()
 {
 	return m_portType;
 }
 
-bool DataPort::open()
+void DataPort::open(const QString& strAddress, const int& number)
 {
-	switch(m_portType)
-	{
-	case DataPort_Type::NULL_PORT:
-		break;
-	case DataPort_Type::NETWORK_PORT:
-		m_networkDataPort->slt_open();
-		break;
-	case DataPort_Type::SERIAL_PORT:
-		m_serialDataPort->slt_open();
-		break;
-	}
-
-	return false;
+	emit sig_open(strAddress, number);
 }
 
 void DataPort::write(const QByteArray &data)
 {
-	Q_UNUSED(data);
-	switch(m_portType)
-	{
-	case DataPort_Type::NULL_PORT:
-		break;
-	case DataPort_Type::NETWORK_PORT:
-		emit sig_wirte(data);
-		break;
-	case DataPort_Type::SERIAL_PORT:
-		emit sig_wirte(data);
-		break;
-	}
+	emit sig_wirte(data);
 }
 
 void DataPort::close()
 {
-	switch(m_portType)
-	{
-	case DataPort_Type::NULL_PORT:
-		break;
-	case DataPort_Type::NETWORK_PORT:
-		m_networkDataPort->slt_close();
-		break;
-	case DataPort_Type::SERIAL_PORT:
-		m_serialDataPort->slt_close();
-		break;
-	}
+	emit sig_close();
 }
